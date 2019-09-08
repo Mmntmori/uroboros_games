84c4164065d870f5f80017340e67fc548b25324f
Evgenii Kozlov <shizzofreni9@gmail.com>
2019-09-08 13:04:15 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/pages/main/js/index.js',
+  output: {
+    filename: './js/index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+    plugins: [
+        new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // all options are optional
+        filename: './css/style.css',
+        chunkFilename: 'chunk-[id].scss',
+        ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+        new HtmlWebpackPlugin({
+            filename: './index.html',
+            template: './src/pages/main/index.pug',
+            inject: true
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            publicPath: './css/',
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+
+            { 
+                test: /\.pug$/,
+                use: [
+                    'html-loader?minimize=false', 
+                    'pug-html-loader?pretty=true'  
+                ],
+            },
+
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: './img/[name].[ext]',
+                            useRelativePath: true          
+                        }
+                    },
+                ],
+            },
+        ]
+    }
+};
