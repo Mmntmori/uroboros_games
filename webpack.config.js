f00dc95c059d9199de87de027537797e0228ea14
Evgenii Kozlov <shizzofreni9@gmail.com>
2019-09-12 22:22:03 +0300

    work in progress: mobile hero

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const File
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-  entry: './src/pages/main/js/index.js',
-  output: {
-    filename: './js/index.js',
-    path: path.resolve(__dirname, 'dist')
-  },
+    mode: 'development',
+    entry: './src/pages/main/js/index.js',
+    output: {
+        filename: './js/index.js',
+        path: path.resolve(__dirname, '/dist')
+    },
     plugins: [
+        new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // all options are optional
@@ -22,52 +26,40 @@ module.exports = {
             template: './src/pages/main/index.pug',
             inject: true
         }),
+        new CopyWebpackPlugin([{
+            from: path.join(__dirname, './pages/main/img/'), 
+            to: "img" }], {
+                
+            }),
+        new UglifyJsPlugin()
     ],
     module: {
-        rules: [
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            // you can specify a publicPath here
-                            // by default it uses publicPath in webpackOptions.output
-                            publicPath: './css/',
-                            hmr: process.env.NODE_ENV === 'development',
-                        },
+        rules: [{
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        // you can specify a publicPath here
+                        // by default it uses publicPath in webpackOptions.output
+                        publicPath: './css/',
+                        hmr: process.env.NODE_ENV === 'development',
                     },
-                    'css-loader',
-                    'sass-loader',
-                ]
-            },
-
-            { 
-                test: /\.pug$/,
-                use: [
-                    'html-loader?minimize=false', 
-                    'pug-html-loader?pretty=true'  
-                ],
-            },
-
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        outputPath: './img/',
-                        options: {
-                            name: './img/[name].[ext]',
-                            useRelativePath: true          
-                        }
-                    },
-                ],
-            },
-        ]
+                },
+                'css-loader',
+                'sass-loader',
+            ]
+        },{ 
+            test: /\.pug$/,
+            use: [
+                'html-loader?minimize=false', 
+                'pug-html-loader?pretty=true'  
+            ],
+        }]
     },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
-        compress: true,
+        compress: false,
         port: 9000
-      }
+    }
 };
